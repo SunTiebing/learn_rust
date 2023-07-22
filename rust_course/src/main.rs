429d7da56fe7a3ab91b69e9ab2060d405fe16253
SunTiebing <1045060705@qq.com>
2023-07-22 17:43:27 +0800

    add homework three example

--- rust_course/src/main.rs
+++ rust_course/src/main.rs
@@ -1,3 +1,4 @@
+use crate::course_three::class::Class;
 use crate::course_three::student::{Student, STUDENTS};
 
 mod course_one;
@@ -7,16 +8,28 @@ mod print_type_one;
 #[allow(dead_code)]
 fn main() {
     // homework one
-    // println!("type one starts print");
-    // print_type_one::print();
+    println!("-----------homework one starts -----------");
+    println!("type one starts print");
+    print_type_one::print();
+    println!("type two starts print");
+    course_one::print_type_two::print();
+    println!("-----------homework one ends -----------");
 
-    // println!("type two starts print");
-    // course_one::print_type_two::print();
-
-    // homework three
+    // homework three examples not include course and society
+    println!("-----------homework two starts -----------");
     let bob = Student::new(String::from("Bob"), 10, None);
     let alice = Student::new(String::from("Alice"), 11, None);
     let mike = Student::new(String::from("Mike"), 9, None);
-
     unsafe { println!("all students: {:?}", STUDENTS) }
+    let mut class = Class::new(1, vec![bob, alice, mike.clone()]);
+    println!("class one : {:?}", class);
+    class.remove_student(mike.num);
+    println!("class one : {:?}", class);
+    class.add_student(mike.num);
+    println!("class one : {:?}", class);
+    class.increase_grade();
+    println!("class one : {:?}", class);
+    Class::remove_class(class.num);
+    println!("class one : {:?}", class);
+    println!("-----------homework two ends -----------");
 }
